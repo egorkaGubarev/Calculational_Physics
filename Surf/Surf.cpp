19083a777d0f6b9f249d49ef162856deb22349f2
egorkaGubarev <gubarev.ed@phystech.edu>
2022-11-05 10:20:22 +0300

    Triangles aren't store pointers to specific nodes any more

--- Surf/Surf.cpp
+++ Surf/Surf.cpp
@@ -33,11 +33,28 @@ struct Node
 
     Node(Point<type> point, const bool is_rigid) : point_{ point }, is_rigid_{ is_rigid } {}
 
+    bool is_connected(Node<type>& other)
+    {
+        Node<type>* other_p = &other;
+        
+        for (Node<type>& connected : connected_nodes_) {
+            Node<type>* connected_p = &connected;
+
+            if (other_p == connected_p) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     void connect(Node<type>& other)
     {
-        connected_nodes_.push_back(other);
-        std::vector<std::reference_wrapper<Node<type>>>& connections = other.connected_nodes_;
-        connections.push_back(*this);
+        if (!is_connected(other)) {
+            connected_nodes_.push_back(other);
+            std::vector<std::reference_wrapper<Node<type>>>& connections = other.connected_nodes_;
+            connections.push_back(*this);
+        }
     }
 
     void disconnect(Node<type>& other)
@@ -111,7 +128,6 @@ struct Triangle
 {
     type length_, square_, max_length_, min_length_;
     Node<type>& node_1_, node_2_, node_3_;
-    Node<type>* max_begin_, * max_end_, * min_begin_, * min_end_;
 
     Triangle(Node<type>& node_1, Node<type>& node_2, Node<type>& node_3):
     node_1_{ node_1}, node_2_{ node_2}, node_3_{ node_3}
@@ -124,72 +140,12 @@ struct Triangle
         const type length_2 = second.length_;
         const type length_3 = theird.length_;
 
+        max_length_ = std::max(length_1, std::max(length_2, length_3));
+        min_length_ = std::min(length_1, std::min(length_2, length_3));
+
         length_ = length_1 + length_2 + length_3;
         const type half_length = length_ / 2;
         square_ = std::sqrt(half_length * (half_length - length_1) * (half_length - length_2) * (half_length - length_3));
-
-        if (length_1 > length_2) {
-            if (length_1 > length_3) {
-                max_length_ = length_1;
-
-                max_begin_ = &node_1_;
-                max_end_ = &node_2_;
-
-                if (length_2 > length_3) {
-                    min_length_ = length_3;
-
-                    min_begin_ = &node_1_;
-                    min_end_ = &node_3_;
-                }
-                else {
-                    min_length_ = length_2;
-
-                    min_begin_ = &node_2_;
-                    min_end_ = &node_3_;
-                }
-            }
-            else {
-                max_length_ = length_3;
-                min_length_ = length_2;
-
-                min_begin_ = &node_2_;
-                min_end_ = &node_3_;
-
-                max_begin_ = &node_1_;
-                max_end_ = &node_3_;
-            }
-        }
-        else {
-            if (length_2 > length_3) {
-                max_length_ = length_2;
-
-                max_begin_ = &node_2_;
-                max_end_ = &node_3_;
-
-                if (length_1 > length_3) {
-                    min_length_ = length_3;
-
-                    min_begin_ = &node_1_;
-                    min_end_ = &node_3_;
-                }
-                else {
-                    min_length_ = length_1;
-
-                    min_begin_ = &node_1_;
-                    min_end_ = &node_2_;
-                }
-            }
-            else {
-                max_length_ = length_3;
-                min_length_ = length_1;
-
-                min_begin_ = &node_1_;
-                min_end_ = &node_2_;
-
-                max_begin_ = &node_1_;
-                max_end_ = &node_3_;
-            }
-        }
     }
 
     bool is_equal(const Triangle<type> other) const
@@ -242,6 +198,77 @@ struct Circuit
         connect_last_and_zero();
     }
 
+    void connect(Node<type>& first, Node<type>& second)
+    {
+        first.connect(second);
+        find_new_triangles(first, second);
+    }
+
+    void connect_last_and_zero()
+    {
+        Node<type>& last = get_last_rigid_node();
+        typename std::list<Node<type>>::iterator begin = std::begin(rigid_nodes_);
+        Node<type>& first = *begin;
+        connect(first, last);
+    }
+
+    // Use when only primary node is created
+    void create_primary_surf()
+    {
+        typename std::list<Node<type>>::iterator begin = std::begin(soft_nodes_);
+        Node<type>& center = *begin;
+
+        for (Node<type>& to_connect : rigid_nodes_) {
+            connect(center, to_connect);
+        }
+    }
+
+    void create_primary_node()
+    {
+        Point<type> center(x_center_, y_center_, z_center_);
+        create_soft_node(center);
+    }
+
+    void create_rigid_node(const Point<type> point)
+    {
+        Node<type> node(point, true);
+        rigid_nodes_.push_back(node);
+    }
+
+    void create_soft_node(const Point<type> point)
+    {
+        Node<type> node(point, false);
+        soft_nodes_.push_back(node);
+    }
+
+    void find_new_triangles(Node<type>& first, Node<type>& second)
+    {
+        const std::vector<std::reference_wrapper<Node<type>>>& first_connect = first.connected_nodes_;
+        const std::vector<std::reference_wrapper<Node<type>>>& second_connect = second.connected_nodes_;
+
+        const Node<type>* const first_ptr = &first;
+        const Node<type>* const second_ptr = &second;
+
+        for (Node<type>& theird : first_connect) {
+            Node<type>* const theird_ptr = &theird;
+
+            if (theird_ptr != second_ptr) {
+
+                for (const Node<type>& node_connect_with_second : second_connect) {
+                    const Node<type>* const node_connect_with_second_ptr = &node_connect_with_second;
+
+                    if (theird_ptr == node_connect_with_second_ptr) {
+                        Triangle<type> new_triangle(first, second, theird);
+                        const bool is_old = is_triangle_exist(new_triangle);
+                        if (!is_old) {
+                            triangles_.push_back(new_triangle);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     void get_borders_coords()
     {
         bool x_max_found = false, x_min_found = false, y_max_found = false, y_min_found = false; bool z_min_found = false, z_max_found = false;
@@ -292,52 +319,6 @@ struct Circuit
         z_center_ = (z_min_ + z_max_) / 2;
     }
 
-    void create_soft_node(const Point<type> point)
-    {
-        Node<type> node(point, false);
-        soft_nodes_.push_back(node);
-    }
-
-    void create_rigid_node(const Point<type> point)
-    {
-        Node<type> node(point, true);
-        rigid_nodes_.push_back(node);
-    }
-
-    void create_primary_node()
-    {
-        Point<type> center(x_center_, y_center_, z_center_);
-        create_soft_node(center);
-    }
-
-    // Use when only primary node is created
-    void create_primary_surf()
-    {
-        typename std::list<Node<type>>::iterator begin = std::begin(soft_nodes_);
-        Node<type>& center = *begin;
-        std::vector<std::reference_wrapper<Node<type>>>& connected_nodes = center.connected_nodes_;
-
-        for (Node<type>& to_connect : rigid_nodes_){
-            connect(center, to_connect);
-        }
-    }
-
-    void get_surf()
-    {
-        get_borders_coords();
-        get_center();
-        create_primary_node();
-        create_primary_surf();
-    }
-
-    Node<type>& get_last_rigid_node()
-    {
-        typename std::list<Node<type>>::iterator end = std::end(rigid_nodes_);
-        typename std::list<Node<type>>::iterator last = std::prev(end);
-        Node<type>& node = *last;
-        return node;
-    }
-
     // Use when you have at least two nodes only
     Node<type>& get_last_but_one_rigid_node()
     {
@@ -346,21 +327,22 @@ struct Circuit
         Node<type>& node = *last;
         return node;
     }
-    
-    Triangle<type>& get_last_triangle()
+
+    // Use when you have at least one node only
+    Node<type>& get_last_rigid_node()
     {
-        typename std::list<Triangle<type>>::iterator end = std::end(triangles_);
-        typename std::list<Triangle<type>>::iterator last = std::prev(end);
-        Triangle<type>& triangle = *last;
-        return triangle;
+        typename std::list<Node<type>>::iterator end = std::end(rigid_nodes_);
+        typename std::list<Node<type>>::iterator last = std::prev(end);
+        Node<type>& node = *last;
+        return node;
     }
 
-    void connect_last_and_zero()
+    void get_surf()
     {
-        Node<type>& last = get_last_rigid_node();
-        typename std::list<Node<type>>::iterator begin = std::begin(rigid_nodes_);
-        Node<type>& first = *begin;
-        connect(first, last);
+        get_borders_coords();
+        get_center();
+        create_primary_node();
+        create_primary_surf();
     }
 
     bool is_triangle_exist(const Triangle<type>& triangle)
@@ -373,38 +355,38 @@ struct Circuit
         return false;
     }
 
-    void find_new_triangles(Node<type>& first, Node<type>& second)
+    std::ofstream& log(std::ofstream& out) const
     {
-        const std::vector<std::reference_wrapper<Node<type>>>& first_connect = first.connected_nodes_;
-        const std::vector<std::reference_wrapper<Node<type>>>& second_connect = second.connected_nodes_;
+        log_rigid(out);
+        log_soft(out);
+        log_connect(out);
 
-        const Node<type>* const first_ptr = &first;
-        const Node<type>* const second_ptr = &second;
-
-        for (Node<type>& theird : first_connect) {
-            Node<type>* const theird_ptr = &theird;
-
-            if (theird_ptr != second_ptr) {
-
-                for (const Node<type>& node_connect_with_second : second_connect) {
-                    const Node<type>* const node_connect_with_second_ptr = &node_connect_with_second;
-
-                    if (theird_ptr == node_connect_with_second_ptr) {
-                        Triangle<type> new_triangle(first, second, theird);
-                        const bool is_old = is_triangle_exist(new_triangle);
-                        if (!is_old) {
-                            triangles_.push_back(new_triangle);
-                        }
-                    }
-                }
-            }
-        }
+        return out;
     }
 
-    void connect(Node<type>& first, Node<type>& second)
+    std::ofstream& log_connect(std::ofstream& out) const
     {
-        first.connect(second);
-        find_new_triangles(first, second);
+        for (const Node<type>& node : soft_nodes_) {
+            const Point<type>& point = node.point_;
+            const std::vector<std::reference_wrapper<Node<type>>>& connect = node.connected_nodes_;
+
+            const type x = point.x_;
+            const type y = point.y_;
+            const type z = point.z_;
+
+            for (const Node<type>& connect_node : connect) {
+                const Point<type>& connect_point = connect_node.point_;
+
+                const type connect_x = connect_point.x_;
+                const type connect_y = connect_point.y_;
+                const type connect_z = connect_point.z_;
+
+                out << x << ' ' << y << ' ' << z << '\n';
+                out << connect_x << ' ' << connect_y << ' ' << connect_z << '\n';
+            }
+        }
+
+        return out;
     }
 
     std::ofstream& log_rigid(std::ofstream& out) const
@@ -437,40 +419,6 @@ struct Circuit
         return out;
     }
 
-    std::ofstream& log_connect(std::ofstream& out) const
-    {
-        for (const Node<type>& node : soft_nodes_) {
-            const Point<type>& point = node.point_;
-            const std::vector<std::reference_wrapper<Node<type>>>& connect = node.connected_nodes_;
-
-            const type x = point.x_;
-            const type y = point.y_;
-            const type z = point.z_;
-
-            for (const Node<type>& connect_node : connect) {
-                const Point<type>& connect_point = connect_node.point_;
-
-                const type connect_x = connect_point.x_;
-                const type connect_y = connect_point.y_;
-                const type connect_z = connect_point.z_;
-
-                out << x << ' ' << y << ' ' << z << '\n';
-                out << connect_x << ' ' << connect_y << ' ' << connect_z << '\n';
-            }
-        }
-
-        return out;
-    }
-
-    std::ofstream& log(std::ofstream& out) const
-    {
-        log_rigid(out);
-        log_soft(out);
-        log_connect(out);
-
-        return out;
-    }
-
     ~Circuit() = default;
 };
 
@@ -491,7 +439,7 @@ Point<type> circle(const type t)
 template <typename type>
 Point<type> deform_circle(const type t)
 {
-    const type vertic_amplit = (type) 0.1;
+    const type vertic_amplit = (type) 1;
     const type vertic_frequency = 10;
 
     const type pi = (type)std::acos(-1);
@@ -547,7 +495,7 @@ Point<type> cardioid(const type t)
 
 int main()
 {
-    const uint dots_on_circ = 10;
+    const uint dots_on_circ = 100;
     std::ofstream aux, file_circle, file_deform_circle, file_eight, file_cardioid;
 
     aux.open("aux_info.txt");
